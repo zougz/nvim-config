c8d2c949361a0035c07629724dfe37e9799fd34f
Noah Malhi <Noah.Malhi@l3harris.com>
2024-08-29 11:19:22 -0400

    Inital commit: Pushing up nvim configuration based on the primeagen

---
+++ nvim/lua/thezougs/init.lua
@@ -0,0 +1,28 @@
+require("thezougs.set")
+require("thezougs.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local TheZougsGroup = augroup('TheZougs', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+	require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+	group = yank_group,
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = 'IncSearch',
+			timeout = 40,
+		})
+	end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
