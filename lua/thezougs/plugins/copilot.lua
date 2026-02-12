655224d3a3aaea7826a557a4e4a28f7a39a67775
Noah Malhi <nmalhi@bostondynamics.com>
2026-02-12 09:57:11 -0500

    Updates lazy config with copilot included

--- lua/thezougs/plugins/copilot.lua
+++ lua/thezougs/plugins/copilot.lua
@@ -1,4 +1,51 @@
 return {
-    "github/copilot.vim",
-    event = "InsertEnter",
+	{
+		"github/copilot.vim",
+		-- Official GitHub plugin
+		lazy = false, -- Recommended not to lazy-load the official plugin for better auth stability
+		config = function()
+			-- Set Alt-L to accept suggestions (to match your previous config)
+			vim.g.copilot_no_tab_map = true
+			vim.keymap.set("i", "<M-l>", 'copilot#Accept("\\<CR>")', {
+				expr = true,
+				replace_keycodes = false,
+			})
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- Dependency updated to official
+			{ "nvim-lua/plenary.nvim" },
+		},
+		build = "make tiktoken",
+		opts = {
+			-- Auto-pull active buffer context for every question
+			selection = function(source)
+				return require("CopilotChat.select").buffer(source)
+			end,
+			window = {
+				layout = "float",
+				width = 0.4,
+				border = "rounded",
+				opts = {
+					wrap = true,
+					linebreak = true,
+				},
+			},
+		},
+		keys = {
+			{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Toggle Copilot Chat" },
+			{
+				"<leader>cq",
+				function()
+					local input = vim.fn.input("Quick Chat (Current Buffer): ")
+					if input ~= "" then
+						require("CopilotChat").ask(input)
+					end
+				end,
+				desc = "Quick Chat with Buffer Context",
+			},
+		},
+	},
 }
--- lua/thezougs/plugins/treesitter.lua
+++ lua/thezougs/plugins/treesitter.lua
@@ -1,70 +1,85 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  
-  -- The 'build' step is necessary for Treesitter to work.
-  build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
 
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {"vim", "c", "lua", "rust", "groovy", "python", "javascript", "typescript", "html", "css", "json"},
-      
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+	-- The 'build' step is necessary for Treesitter to work.
+	build = ":TSUpdate",
 
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"vim",
+				"c",
+				"cpp",
+				"vimdoc",
+				"vim",
+				"lua",
+				"rust",
+				"groovy",
+				"python",
+				"javascript",
+				"typescript",
+				"html",
+				"css",
+				"json",
+			},
 
-      -- Configuration for `nvim-treesitter-textobjects`
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            -- Text objects for function calls/definitions, classes, loops, etc.
-            -- e.g., 'af' (around function), 'if' (inside function)
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- Re-use vim's jump list
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-      },
-      
-      indent = { enable = true },
-    }
-    
-    if #vim.api.nvim_list_uis() > 0 then
-      vim.cmd('silent! TSUpdateSync')
-    end
-  end,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+
+			-- Automatically install missing parsers when entering buffer
+			auto_install = true,
+
+			highlight = {
+				-- `false` will disable the whole extension
+				enable = false,
+				additional_vim_regex_highlighting = false,
+			},
+
+			-- Configuration for `nvim-treesitter-textobjects`
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						-- Text objects for function calls/definitions, classes, loops, etc.
+						-- e.g., 'af' (around function), 'if' (inside function)
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- Re-use vim's jump list
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+			},
+
+			indent = { enable = true },
+		})
+
+		if #vim.api.nvim_list_uis() > 0 then
+			vim.cmd("silent! TSUpdateSync")
+		end
+	end,
 }
